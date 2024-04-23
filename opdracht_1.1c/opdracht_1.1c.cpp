1b019db9c7ff9b7d1ed40d137aee5faf909eb119
Kerr Beeldens <kerrbeeldens@outlook.com>
2024-04-23 11:48:53 +0200

    Clarified the code of exercise 1.1c

--- opdracht_1.1c/opdracht_1.1c.cpp
+++ opdracht_1.1c/opdracht_1.1c.cpp
@@ -7,6 +7,8 @@ using namespace cv;
 using namespace std;
 
 void main() {
+	/* Note: loading in an image, video or webcamfeed can not be done at the same time.
+	comment out everything except for the operation you want to perform. */
 
 	// Load in an image
 	string imagePath = "../Resources/example_photo.jpg";
@@ -15,25 +17,25 @@ void main() {
 	waitKey(0);
 
 	// Load in a video
-	//string videoPath = "../Resources/example_video.mp4";
-	//VideoCapture cap(videoPath);
-	//Mat video_frame;
+	string videoPath = "../Resources/example_video.mp4";
+	VideoCapture cap(videoPath);
+	Mat video_frame;
 
-	//while (true) {
+	while (true) {
 
-	//	cap.read(video_frame);
-	//	imshow("Video Frame", video_frame);
-	//	waitKey(40); // 1000 / framerate (25 fps) 
-	//}
+		cap.read(video_frame);
+		imshow("Video Frame", video_frame);
+		waitKey(40); // 1000 / framerate (25 fps) 
+	}
 
 	// Load in a webcam feed
-	//VideoCapture cap(0);
-	//Mat webcam;
+	VideoCapture cap(0);
+	Mat webcam;
 
-	//while (true) {
+	while (true) {
 
-	//	cap.read(webcam);
-	//	imshow("Webcam Feed", webcam);
-	//	waitKey(1);
-	//}
+		cap.read(webcam);
+		imshow("Webcam Feed", webcam);
+		waitKey(1);
+	}
 }
