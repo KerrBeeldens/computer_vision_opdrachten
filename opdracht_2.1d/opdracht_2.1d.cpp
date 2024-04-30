ff2af8031791d7b6f3b663a0b6284a265e49ae4e
Kerr Beeldens <ksv.beeldens@student.avans.nl>
2024-04-30 18:55:10 +0200

    Finished Exercise 2.1d

---
+++ opdracht_2.1d/opdracht_2.1d.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void main() {
+	// Load in an image
+	string imagePath = "../Resources/example_photo.jpg";
+	Mat img = imread(imagePath);
+
+	// Convert to grayscale
+	Mat grayscale;
+	cvtColor(img, grayscale, COLOR_BGR2GRAY);
+
+	// Apply Gaussian blur
+	Mat gaussian;
+	GaussianBlur(grayscale, gaussian, Size(5, 5), 1);
+
+	// Display the result
+	namedWindow("Image", WINDOW_NORMAL);
+	imshow("Image", grayscale);
+
+	waitKey(0);
+}
